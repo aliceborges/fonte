5a6beac3a16adf171cbb1d086525ff07bf902b3c
Alice Borges <aliceborges@outlook.com.br>
2017-12-07 20:12:09 -0200

    reparos

--- fonte/views/index.py
+++ fonte/views/index.py
@@ -4,7 +4,7 @@ from . import visitor_cookie_handler
 
 def index(request):
     #request.session.set_test_cookie()
-    category_list = Category.objects.order_by('name')[:5]
+    category_list = Category.objects.order_by('name')[:6]
     font_list = Font.objects.order_by('views')[:32]
 
     context_dict = {'categories':category_list,
